804fc57d685b5f6d5cbc5264c6d4868c1b0a6844
thehutman <thehutman@d8ef74a6-7c47-47f7-9d7e-6d18641b8ca3>
2013-10-17 10:55:43 +0000

    Initial upload from local repository.

---
+++ src/local/thehutman/worldgen/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * This package contains a Bukkit plugin WorldGen, used to help generate Minecraft structures
+ * in an existing world.
+ * 
+ * @author Huttinger
+ *
+ */
+package local.thehutman.worldgen;
